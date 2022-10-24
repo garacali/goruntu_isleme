dafad8c90d7c7e6392032fca8f0efbd8ce281188
garacali <karacalifurkan4@gmail.com>
2022-10-24 14:48:42 +0300

    histogram
    kütüphane kullanmadan gri seviye görüntünün histogram hesabı yapılması

---
+++ histogramhesabi/main.py
@@ -0,0 +1,39 @@
+import  cv2
+import  numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+from numpy import zeros, shape
+
+foto=cv2.imread("indir.jpg",0)
+
+cv2.imshow("resim",foto)
+
+
+histogram=zeros(256)
+
+[satir,sutun]=shape(foto)
+
+for row in range(0,satir):#burada satır kontrolu yapıyoruz
+    for column in range(0,sutun):#burada belirli satırın sütunlarını geziyoruz
+
+        i=foto[row,column]#ilgili piksel değerini alıyoruz
+
+        histogram[i]=histogram[i]+1##sonra aldığımız piksel seviyesini 1 arttırıyoruz
+
+
+
+print("histogram dizisi şu şekildedir:")
+
+for i in range(0,256):
+    print(histogram[i])
+
+
+
+
+
+
+cv2.waitKey()
+
+
+
+
